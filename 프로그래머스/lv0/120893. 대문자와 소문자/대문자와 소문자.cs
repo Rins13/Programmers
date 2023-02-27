3948370a9e603c1f837ebf5963c8f8b31d771fdd
Rins <70185944+Rins13@users.noreply.github.com>
2023-02-27 11:03:19 +0900

    Update 대문자와 소문자.cs
    코드 정렬

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.cs
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.cs
@@ -3,14 +3,13 @@ using System;
 public class Solution {
     public string solution(string my_string) {
         string answer = "";
-		foreach(char ch in my_string)
-		{
-			if (char.IsUpper(ch))
+	foreach(char ch in my_string)
+	{
+		if (char.IsUpper(ch))
 	            answer += char.ToLower(ch);
-            else
+		else
 	            answer += char.ToUpper(ch);
-
-		}
-		return answer;
+	}
+	return answer;
     }
 }
