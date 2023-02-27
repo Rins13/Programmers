a93cefe645370ba3215db74eb19c33a446be8d63
Rins <70185944+Rins13@users.noreply.github.com>
2023-02-27 11:00:38 +0900

    [level 0] Title: 대문자와 소문자, Time: 1.29 ms, Memory: 31.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution {
+    public string solution(string my_string) {
+        string answer = "";
+		foreach(char ch in my_string)
+		{
+			if (char.IsUpper(ch))
+	            answer += char.ToLower(ch);
+            else
+	            answer += char.ToUpper(ch);
+
+		}
+		return answer;
+    }
+}
