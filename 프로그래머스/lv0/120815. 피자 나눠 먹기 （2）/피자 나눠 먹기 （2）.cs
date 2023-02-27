02ad4b28af46bb3f26d2782de3d81be3b508773b
Rins <70185944+Rins13@users.noreply.github.com>
2023-02-27 14:15:18 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.23 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        
+		for (int i = 1; i <= 100; i++)
+		{
+			if ((i * 6) % n == 0)
+				return i;
+		}
+		return -1;
+    }
+}
