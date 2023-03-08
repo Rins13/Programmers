9e8b999815d5ecaab26f0b2c4a77fa22c3b0ddc4
Rins <70185944+Rins13@users.noreply.github.com>
2023-03-08 17:14:19 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.74 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.cs
+++ 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.cs
@@ -1,39 +1,7 @@
-using System.Collections.Generic;
+using System.Linq;
 
 public class Solution {
     public int solution(int[] sides) {
-		List<int> addLegs = new List<int>();
-
-		int b = sides[0], s = sides[1];
-		if (sides[0] < sides[1])
-		{
-			b = sides[1];
-			s = sides[0];
-		}
-
-		int other = b;
-		while(true)
-		{
-			if (other + s > b &&  !addLegs.Contains(other))
-				addLegs.Add(other);
-			else if(other < 1)
-				break;
-
-			other--;
-		}
-
-		other = (s+b) -1;
-		while (true)
-		{
-			if (other > b && !addLegs.Contains(other))
-				addLegs.Add(other);
-			else
-				break;
-
-			other--;
-		}
-
-		return addLegs.Count;
-
+		return 2 * sides.Min() - 1;
     }
 }
