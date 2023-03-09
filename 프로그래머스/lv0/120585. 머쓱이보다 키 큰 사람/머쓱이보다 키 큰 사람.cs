61b3078b2248df4debf1e60772fa307d216b438d
Rins <70185944+Rins13@users.noreply.github.com>
2023-03-09 16:15:55 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.16 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(int[] array, int height) {
+			int answer = 0;
+
+			foreach (int num in array)
+				if (num > height)
+					answer++;
+
+			return answer;
+    }
+}
