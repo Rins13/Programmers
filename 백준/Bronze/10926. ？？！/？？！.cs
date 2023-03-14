2e2dd18d9a7183bfd57e71f1e0f46102dd18e725
Rins <70185944+Rins13@users.noreply.github.com>
2023-03-14 13:50:05 +0900

    [Bronze V] Title: ??!, Time: 64 ms, Memory: 5376 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cs
@@ -0,0 +1,11 @@
+namespace ConsoleApp2
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			string id = System.Console.ReadLine();
+			System.Console.WriteLine($"{id}??!");
+		}
+	}
+}
