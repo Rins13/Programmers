9c586e86cbbe40dbfb21693d80f8264e5cfc65c1
Rins <70185944+Rins13@users.noreply.github.com>
2023-03-15 13:15:18 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.80 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public int solution(string s) {
+		string[] numbers = new string[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+		for(int i = 0; i< numbers.Length;i++)
+			s = s.Replace(numbers[i], i.ToString());
+
+		return int.Parse(s);
+
+    }
+}
