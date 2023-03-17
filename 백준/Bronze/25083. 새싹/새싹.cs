8cbff407de93dc41ec7d5f23f685809b031b6642
Rins <70185944+Rins13@users.noreply.github.com>
2023-03-18 01:35:10 +0900

    [Bronze V] Title: 새싹, Time: 48 ms, Memory: 5336 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cs
@@ -0,0 +1,10 @@
+namespace ConsoleApp
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			System.Console.WriteLine("         ,r'\"7\nr`-_   ,'  ,/\n \\. \". L_r'\n   `~\\/\n      |\n      |\n");
+		}
+	}
+}
