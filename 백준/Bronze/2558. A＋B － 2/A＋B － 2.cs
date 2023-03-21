92121124bdb5e7e171d9d8e59794f6ace3b98ae3
Rins <70185944+Rins13@users.noreply.github.com>
2023-03-21 09:48:10 +0900

    [Bronze V] Title: A+B - 2, Time: 64 ms, Memory: 5484 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cs
@@ -0,0 +1,12 @@
+namespace ConsoleApp
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			int a = int.Parse(System.Console.ReadLine());
+			int b = int.Parse(System.Console.ReadLine());
+			System.Console.WriteLine(a + b);
+		}
+	}
+}
