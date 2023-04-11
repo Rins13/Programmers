407bc599c39cedb6a59fb3397abb4b9cb8144e5b
Rins <70185944+Rins13@users.noreply.github.com>
2023-04-11 10:10:00 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 64 ms, Memory: 5504 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.cs
@@ -0,0 +1,11 @@
+namespace ConsoleApp
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			double n = int.Parse(System.Console.ReadLine());
+			System.Console.WriteLine(n*4);
+		}
+	}
+}
